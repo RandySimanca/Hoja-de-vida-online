bb53b9d38b893cb73a66f36a623fd736e31e75c0
Randy Simanca <randysimancamercado@gmail.com>
2025-08-19 14:52:46 -0500

    conexion con heroku

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,24 @@ dotenv.config();
 connectDB(); // ← esta línea sí conecta usando tu configuración personalizada
 mongoose.connect("mongodb+srv://randysimancamercado2:Valeria1324@clustermiapp.z0bbfnk.mongodb.net/baseDeDatosHV", {useNewUrlParser: true})
 
+
+// usar estáticos cuando esta en modo produccion //
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get("*", (req, res) => {
+      res.sendFile((__dirname + "/frontend/build/index.html"));
+  })
+  }
+  
+  
+  // cambio de puerto en heroku
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+  port = 5000;
+  }
+  ////////// 2 fragmentos necesarios para implementar heroku
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
