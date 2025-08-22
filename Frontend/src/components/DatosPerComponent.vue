fbfa8251a3f741bb611fd702f3ec9da2496fe364
RandySimanca <randysimancamercado@gmail.com>
2025-08-22 15:11:20 -0500

    Update DatosPerComponent.vue

--- Frontend/src/components/DatosPerComponent.vue
+++ Frontend/src/components/DatosPerComponent.vue
@@ -191,8 +191,7 @@
         <div class="form-group col-2">
           <label>FECHA Y LUGAR DE NACIMIENTO</label>
           <div style="display: flex; flex-wrap: wrap">
-                        <div class="form-group" 
-                            style="margin-right: 5px">
+                        <div class="form-group col-2">
                               <label for="dia-nac">DÍA</label>
                               <input
                                 type="text"
@@ -201,7 +200,7 @@
                                 v-model="diaNac"
                               />
                         </div>
-            <div class="form-group" style="margin-right: 5px">
+            <div class="form-group col-2">
               <label for="mes-nac">MES</label>
               <input
                 type="text"
@@ -210,7 +209,7 @@
                 v-model="mesNac"
               />
             </div>
-            <div class="form-group" style="margin-right: 5px">
+            <div class="form-group col-2" >
               <label for="ano-nac">AÑO</label>
               <input
                 type="text"
