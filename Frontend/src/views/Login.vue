7a48120ac0d8dde84c15644fb362d8b8a79dfba7
RandySimanca <randysimancamercado@gmail.com>
2025-08-28 10:34:58 -0500

    Update Login.vue
    se corrige la palabra Registrarme por Registrate

--- Frontend/src/views/Login.vue
+++ Frontend/src/views/Login.vue
@@ -43,7 +43,7 @@
                 ? "Registrando..."
                 : "Ingresando..."
               : modoRegistro
-              ? "Registrarme"
+              ? "Registrate"
               : "Entrar"
           }}
         </button>
